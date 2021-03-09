6ee95ecd86935681e14850af9f6308ea58537635
wlllyfor <958167224@qq.com>
2021-03-09 10:25:18 +0800

    feat：响应数据支持泛型

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -9,13 +9,13 @@ import axios from '../../src/index'
 //   }
 // })
 
-axios.request({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hello'
-  }
-})
+// axios.request({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hello'
+//   }
+// })
 
 // axios.get('/extend/get')
 
@@ -30,3 +30,38 @@ axios.request({
 // axios.put('/extend/put', { msg: 'put' })
 
 // axios.patch('/extend/patch', { msg: 'patch' })
+
+// axios({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hi'
+//   }
+// })
+
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
+
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err))
+}
+
+
+async function test() {
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result.name)
+  }
+}
+
+test()
--- src/axios.ts
+++ src/axios.ts
@@ -6,8 +6,6 @@ function createInstance(): AxiosInstance {
   const context = new Axios()
   const instance = Axios.prototype.request.bind(context)
 
-  console.log(context)
-
   extend(instance, context)
 
   return instance as AxiosInstance
